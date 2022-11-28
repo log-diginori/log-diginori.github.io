6de11aefd742ef3d4332d232b5269681ca1d3d32
becky2sawyer <becky2sawyer@gmail.com>
2022-11-29 00:12:40 +0900

    He's constantly pursuing engineering

--- docs/assets/index.html.f4dd6751.js
+++
@@ -1,1 +0,0 @@
-const e=JSON.parse('{"key":"v-15be81bb","path":"/logs/tech/sre/","title":"SRE","lang":"en-US","frontmatter":{},"excerpt":"","headers":[{"level":3,"title":"monitoring vs observability","slug":"monitoring-vs-observability","link":"#monitoring-vs-observability","children":[]}],"git":{"updatedTime":1669609436000},"filePathRelative":"logs/tech/sre/index.md"}');export{e as data};
