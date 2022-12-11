489f2e7c3adcdc0dff9b8c9eb93910a35ba724ba
becky2sawyer <becky2sawyer@gmail.com>
2022-12-11 13:35:46 +0900

    Agile Fighter, Draft Completion

---
+++ docs/assets/index.html.940933da.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-fe54f7b6","path":"/logs/tech/agile/","title":"AGILE 갑론을박","lang":"ko-KR","frontmatter":{},"excerpt":"","headers":[{"level":3,"title":"별을 보려면 태양을 꺼라","slug":"별을-보려면-태양을-꺼라","link":"#별을-보려면-태양을-꺼라","children":[]}],"git":{"updatedTime":null},"filePathRelative":"logs/tech/agile/index.md"}');export{e as data};
